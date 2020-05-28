172b5fde34bf7e685104a059f1c461ca9157c849
psuraj776 <33577824+psuraj776@users.noreply.github.com>
2020-05-29 00:54:07 +0530

    Update Depth_First_Search.java

--- GRAPH/Depth_First_Search.java
+++ GRAPH/Depth_First_Search.java
@@ -1,2 +1,28 @@
-class DFS{
+/*
+g : adjacency list of graph
+N : number of vertices
+
+return a list containing the DFS traversal of the given graph
+*/
+
+class DFS_Traversal
+{   ArrayList<Integer> result;
+    boolean[] visited;
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> g, int N)
+    {  visited=new boolean[N];
+       result=new ArrayList<>();
+       helper(g,0,N);
+       return result;
+       // add your code here
+    }
+    public void helper(ArrayList<ArrayList<Integer>> g, int temp){
+        if(visited[temp]) return;
+        result.add(temp);
+        visited[temp]=true;
+        for(int A:g.get(temp)){
+            if(!visited[A]){
+                helper(g,A);
+            }
+        }
+    }
 }
