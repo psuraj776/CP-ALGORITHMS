1bbd20723136a064a66b16c24ec1702b1e6ad770
psuraj776 <33577824+psuraj776@users.noreply.github.com>
2024-08-05 11:29:30 -0700

    Create ExtendEuclidAlgo.java

---
+++ Algorithm/ExtendedEuclideanAlgorithms/ExtendEuclidAlgo.java
@@ -0,0 +1,19 @@
+/*
+*/
+
+import java.util.*;
+import java.io.*;
+
+class ExtendEuclid{
+  public int extendEuclid(int A, int B){
+  }
+}
+
+class Main{
+  public static void main(String[] args){
+    ExtendEuclid extend_euclid = new ExtendEuclid();
+    int A = 66;
+    int B = 22;
+    System.out.println("GCD of "+A" and "+B+" is "+extend_euclid.extendEuclid(A, B))
+  }
+}
